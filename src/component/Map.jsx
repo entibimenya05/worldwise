8e309c98cf531319f5a3bad128624127b3cc2478
emmanuel <entibimenya03@gmail.com>
2025-06-09 12:58:30 +0100

    interacting with the map by clicking on it

--- src/component/Map.jsx
+++ src/component/Map.jsx
@@ -1,16 +1,30 @@
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useState, useEffect } from "react";
 import { useCities } from "../contexts/CitiesContext";
 function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   const { cities } = useCities();
   //in order to read the lat and lng,useSearchParams
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+  //we need to synchronize the above so we use an effect
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
   return (
     //programatic navigation: moving to a new page without clicking on any link
     //usually moving to a Form or after submitting a form
@@ -19,7 +33,9 @@ function Map() {
     <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        //to implement the below, you need to create a new function: ChangeCenter
+
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -38,9 +54,24 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+//when you click somewhere on the map, to open the form
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
 
 export default Map;
